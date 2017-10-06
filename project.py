fecf46d72bd165b1628b317a17ffae4618c8e90e
thomas ngo <tngo0508@gmail.com>
2017-10-06 01:14:29 -0700

    add text to binary function

--- project.py
+++ project.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 from __future__ import print_function
-import sys
-import argparse
+import sys, os, argparse
 from PIL import Image
 
 def text_to_bin(text):
-    return ''.join(format(ord(x), 'b') for x in st)
+    return ''.join(format(ord(x), 'b') for x in text)
+
 def main():
-    pass
+    im = Image.open("testImage.png")
+    width, height = im.size
+    print(width, height)
+    print(im.format, im.size, im.mode)
+    foo = text_to_bin("foo")
+    print(foo)
+
 
 if __name__ == "__main__":
-    Main()
+    main()
