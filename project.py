cc7218ada84a7b19027674b366a866da27fd312e
thomas ngo <tngo0508@gmail.com>
2017-10-05 21:04:42 -0700

    initial commit

---
+++ project.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+import sys
+import argparse
+from PIL import Image
+
+def text_to_bin(text):
+    return ''.join(format(ord(x), 'b') for x in st)
+def main():
+    pass
+
+if __name__ == "__main__":
+    Main()
